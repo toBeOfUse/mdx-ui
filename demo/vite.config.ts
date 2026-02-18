22930452d7212a6699ba61b69350c1f2b9868c80
Mitch J <mjacovet@kent.edu>
2026-02-18 13:04:19 -0500

    github pages attempt 2

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -3,6 +3,13 @@ import react from '@vitejs/plugin-react'
 import mdx from '@mdx-js/rollup'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [mdx(), react()],
+export default defineConfig(() => {
+  const isGitHubPages = process.env.GITHUB_PAGES === 'true';
+  const repo = process.env.GITHUB_REPOSITORY?.split('/')[1];
+  const base = isGitHubPages && repo ? `/${repo}/` : '/';
+
+  return {
+    base,
+    plugins: [mdx(), react()],
+  };
 })
